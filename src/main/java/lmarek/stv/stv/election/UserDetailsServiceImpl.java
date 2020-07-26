221f6a1e3d0f04fec2492d6aa84b009192dd977c
Łukasz Marek <marek.lucas@gmail.com>
2020-07-26 18:00:03 +0200

    fetching grants from repository

--- src/main/java/lmarek/stv/stv/election/UserDetailsServiceImpl.java
+++ src/main/java/lmarek/stv/stv/election/UserDetailsServiceImpl.java
@@ -1,16 +1,22 @@
 package lmarek.stv.stv.election;
 
+import lmarek.stv.stv.user.AuthorizationEntity;
 import lmarek.stv.stv.user.UserEntity;
 import lmarek.stv.stv.user.UserRepository;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -24,15 +30,24 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         final Optional<UserEntity> userEntity = userRepository.findUserByEmail(username);
-        final Optional<UserDetails> userDetails = userEntity
-                .map(entity -> new User(username, entity.getPassword(), Collections.emptyList()));
+        final Optional<UserDetails> userDetails = userEntity.map(this::convertToUserDetails);
 
         if(userDetails.isEmpty()){
             throw new UsernameNotFoundException(String.format(ERROR_MSG_TEMPLATE, username));
         }
 
         return userDetails.get();
+    }
+
+    private UserDetails convertToUserDetails(UserEntity userEntity){
+        final List<GrantedAuthority> authorizations = userEntity.getAuthorizations()
+                .stream()
+                .map(AuthorizationEntity::getName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return new User(userEntity.getEmail(), userEntity.getPassword(), authorizations);
     }
 }
